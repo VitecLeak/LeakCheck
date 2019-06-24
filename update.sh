86231b649aab8280f8d8166a3d7da6ab82c928d3
Thach <thachle@metran.co.jp>
2019-06-24 16:21:02 +0700

    Update 24/6/2019

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 cd /usr/bin/
 sudo -S git clone https://github.com/VitecLeak/LeakCheck.git
 cd /usr/bin/LeakCheck
-sudo -S rm -rf /usr/bin/LeakCheck_V1.2
 sudo -S cp update.sh /usr/bin/
 sudo -S cp LeakCheck_V1.2 /usr/bin/
 sudo -S cp update.desktop /home/pi/Desktop/Leak-Check/

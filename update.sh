69beb87d3b08826bde714781758e6cd337e654ba
Thach <thachle@metran.co.jp>
2019-06-24 15:43:10 +0700

    Update 24/6/2019

--- update.sh
+++ update.sh
@@ -8,5 +8,6 @@ sudo -S cp LeakCheck_V1.2 /usr/bin/
 sudo -S cp update.desktop /home/pi/Desktop/Leak-Check/
 
 sudo -S chmod 755 /usr/bin/LeakCheck_V1.2
-sudo -s chmod 755 /usr/bin/update.sh
+sudo -S chmod 755 /usr/bin/update.sh
+sudo -S chmod 755 /home/pi/Desktop/Leak-Check/update.desktop
 sudo -S rm -rf /usr/bin/LeakCheck

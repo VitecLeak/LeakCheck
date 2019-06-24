e92425038dd18113fb873443c70fbd35d597def8
Thach <thachle@metran.co.jp>
2019-06-24 14:45:39 +0700

    Update 24/6/2019

--- update.sh
+++ update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#sudo -S git clone https://github.com/HoangVietCuong/LeakCheck.git
-#cd /usr/bin/LeakCheck/
-#sudo -S git pull
-#sudo -S rm -rf /usr/bin/LeakCheck_V1.2
-#sudo -S cp update.sh /usr/bin/
-#sudo -S cp LeakCheck_V1.2 /usr/bin/
-#sudo -S cp update.desktop /home/pi/Desktop/Leak-Check/
+sudo -S git clone https://github.com/VitecLeak/LeakCheck.git
+sudo -S rm -rf /usr/bin/LeakCheck
+cd /usr/bin/
+sudo -S git pull
+cd /usr/bin/LeakCheck
+sudo -S rm -rf /usr/bin/LeakCheck_V1.2
+sudo -S cp update.sh /usr/bin/
+sudo -S cp LeakCheck_V1.2 /usr/bin/
+sudo -S cp update.desktop /home/pi/Desktop/Leak-Check/
 
-#Update 18/4/2019 Thach
-sudo  rm -rf /usr/bin/LeakCheck_V1.2
-cd /home/pi/Desktop
-sudo  cp LeakCheck_V1.2 /usr/bin/
-sudo  rm -rf LeakCheck_V1.2
+sudo -S chmod 755 /usr/bin/LeakCheck_V1.2
+sudo -s chmod 755 /usr/bin/update.sh
